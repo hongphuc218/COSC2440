29a1e98e80425922e6f2d436a937b3392e108b81
hongphuc218 <hongphuc218@gmail.com>
2022-03-24 10:45:37 +0700

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Student {
+    String name;
+    int id;
+    
+
+
+}
